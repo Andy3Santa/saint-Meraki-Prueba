fc7c5a35d3d73587c063ed3c3e565c04fa65ae1d
Andres Santamaria <andresantamaria333@gmail.com>
2020-08-17 11:29:07 -0500

    cambio en post con mensaje enviando secret

--- src/appp.js
+++ src/appp.js
@@ -94,7 +94,7 @@ app.post('/about', function(req, res) {
 		res.status(201).send(req.body);
 	} else {
 		console.log('Secret was invalid');
-		res.status(201).send('Secret was invalid no message');
+		res.status(201).send(secret);
 	}
 	
 });
